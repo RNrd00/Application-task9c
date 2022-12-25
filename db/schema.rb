2d4aa84bf6d16c465fb447bdc837a814a0248362
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2022-12-25 13:04:42 +0000

    [Finish]Application-task8c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_24_021629) do
+ActiveRecord::Schema.define(version: 2022_12_25_125722) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2022_12_24_021629) do
   create_table "group_users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   create_table "groups", force: :cascade do |t|
